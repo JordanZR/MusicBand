c2c810bb12ed1a0d7e4f5506a691af3d485bb30c
JordanZ <jordan.zelayahs@gmail.com>
2023-03-30 13:17:37 -0600

    Footer added

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    IndexComponent
+    IndexComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
