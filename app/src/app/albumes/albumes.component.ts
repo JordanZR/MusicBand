3f1a93c75014df364fe008a4907dd3930ec68ac3
JordanZ <jordan.zelayahs@gmail.com>
2023-03-30 18:43:38 -0600

    Added Albumes media player

--- app/src/app/albumes/albumes.component.ts
+++ app/src/app/albumes/albumes.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import songsJSON from '../../assets/songs.json'
 @Component({
   selector: 'app-albumes',
   templateUrl: './albumes.component.html',
   styleUrls: ['./albumes.component.css']
 })
-export class AlbumesComponent {
-
+export class AlbumesComponent implements OnInit{
+  audio = document.createElement("audio");
+  songs:any[] = []
+  playedSong:string = ""
+  playAudio(song:string):void{
+    if(this.playedSong != song){
+      this.audio.src = song
+      this.playedSong = song
+    }
+    this.audio.play();
+  }
+  pauseAudio():void{
+    this.audio.pause()
+  }
+  ngOnInit() {
+    document.body.appendChild(this.audio);
+    for(let i = 0; i<songsJSON.length;i++){
+      this.songs.push(songsJSON[i])
+    }
+  }
 }
