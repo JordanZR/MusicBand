cb909f8e79c0e08d789a7ddbf65e6205c5f1b537
JordanZ <jordan.zelayahs@gmail.com>
2023-05-10 15:54:47 -0600

    Added imports

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,6 +12,8 @@ import { MerchComponent } from './merch/merch.component';
 import { GaleriaComponent } from './galeria/galeria.component';
 import { MiembrosComponent } from './miembros/miembros.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { ContactoComponent } from './contacto/contacto.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
