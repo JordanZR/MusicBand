7c627cb6f2853ebd01777b9935ba26d2633ebd62
JordanZ <jordan.zelayahs@gmail.com>
2023-05-10 15:46:00 -0600

    Changed form to Div and email service working perfectly

--- app/src/app/contacto/contacto.component.ts
+++ app/src/app/contacto/contacto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2'
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+
+  public nombre:string = ""
+  public apellido:string = ""
+  public correo:string = ""
+  public telefono:string = ""
+  public empresa:string = ""
+  public contenido:string = ""
+  constructor(private emailService: EmailService) {
+  }
+
+  send():any{
+    Swal.fire(
+      'Solicitud enviada!',
+      'Nos pondremos en contacto contigo al a brevedad posible!',
+      'success'
+    )
+
+    this.emailService.sendEmail(
+      this.correo,
+      this.nombre,
+      this.apellido,
+      this.telefono,
+      this.empresa,
+      this.contenido
+    ).subscribe()
+
+    this.nombre = ""
+    this.apellido = ""
+    this.correo = ""
+    this.telefono = ""
+    this.empresa = ""
+    this.contenido = ""
+  }
 
 }
